f157957f751dd957973b956f1ac749878435a138
Nisrine-C <nisrinechakir237@gmail.com>
2025-05-18 22:43:21 +0100

    security

--- src/main/java/ma/chakir/digitalBanking/services/BankAccountService.java
+++ src/main/java/ma/chakir/digitalBanking/services/BankAccountService.java
@@ -9,12 +9,19 @@ import java.util.List;
 
 public interface BankAccountService {
     CustomerDTO saveCustomer(CustomerDTO customerDTO);
+
     CurrentBankAccountDTO saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+
     SavingBankAccountDTO saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
+
     List<CustomerDTO> listCustomers();
+
     BankAccountDTO getBankAccount(String accountId) throws BankAccountNotFoundException;
+
     void debit(String accountId, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
+
     void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
+
     void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
 
     List<BankAccountDTO> bankAccountList();
